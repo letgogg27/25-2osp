37d3d262eb5f506f1926fce706319e35b899e32d
chaeeun604 <ce7828@ewha.ac.kr>
2025-10-12 20:25:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
-@app.route('/')
-def hello():
-  return render_template("index.html")
+# 더미 상품 (이미지 파일은 static/images/ 에 저장)
+PRODUCTS = [
+    {"id": 1,  "name": "이화 로고 자수 반팔 티셔츠", "price": 19900,  "img": "tshirt.png",  "cat": "의류"},
+    {"id": 2,  "name": "이화 반다나",               "price": 14900,  "img": "bandana.png", "cat": "잡화"},
+    {"id": 3,  "name": "이화 피그먼트 캡",          "price": 24900,  "img": "hat.png",     "cat": "잡화"},
+    {"id": 4,  "name": "진공 청소기",               "price": 99999,  "img": "vacuum.png",  "cat": "가전"},
+    {"id": 5,  "name": "이화 PP 포스트잇",          "price": 4000,   "img": "postit.png",  "cat": "문구"},
+    {"id": 6,  "name": "텀블러",                    "price": 19000,  "img": "tumbler.png", "cat": "생활"},
+    {"id": 7,  "name": "이화 로고 자수 반팔 티(그린)", "price": 19900,  "img": "tshirt.png",  "cat": "의류"},
+    {"id": 8,  "name": "이화 반다나(블랙)",          "price": 14900,  "img": "bandana.png", "cat": "잡화"},
+    {"id": 9,  "name": "이화 피그먼트 캡(블루)",     "price": 25900,  "img": "hat.png",     "cat": "잡화"},
+    {"id": 10, "name": "진공 청소기 Pro",           "price": 129000, "img": "vacuum.png",  "cat": "가전"},
+]
 
+PAGE_SIZE = 8
+
+def render_list():
+    page = int(request.args.get("page", 1))
+    start = (page - 1) * PAGE_SIZE
+    end   = start + PAGE_SIZE
+    total_pages = (len(PRODUCTS) + PAGE_SIZE - 1) // PAGE_SIZE
+    return render_template(
+        "list.html",
+        products=PRODUCTS[start:end],
+        page=page,
+        total_pages=total_pages
+    )
+
+# 홈 = 리스트
+@app.route("/", strict_slashes=False)
+def home():
+    return render_list()
+
+# /list 로 접근해도 동일
+@app.route("/list", strict_slashes=False)
+def product_list():
+    return render_list()
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=5000 ,debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
