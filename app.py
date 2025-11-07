fe837404f688777393a30165589a0107d6275ffa
Eloise Kha <105734098+Eloisekha@users.noreply.github.com>
2025-11-07 18:49:22 +0900

    connect item_detail to list html

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import hashlib
 from database import DBhandler
 import sys
 
+from flask import abort
+
 app = Flask(__name__, template_folder="templates", static_folder="static")
 app.secret_key="some-secret"
 
